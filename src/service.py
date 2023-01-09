de00e549c67c8b741d19942d4b9bdf469fe5c2ca
Maksat Dovletov <mbdovletov@gmail.com>
2023-01-10 00:05:05 +0300

    [MO80-209M-Dovletov] [Lab is finished]

--- src/service.py
+++ src/service.py
@@ -5,7 +5,7 @@ Web-service with API
 """
 import os
 import unicodedata
-
+import pickle
 
 import numpy as np
 from flask import Flask, render_template, request
@@ -16,17 +16,17 @@ from src.utils import conf, logger, MessagesDB
 db = MessagesDB(conf)
 db.init_db()
 
-def load_model(model_path):
+def load_model(model_path, vectorizer_path):
     # ------ YOUR CODE HERE ----------- #
     # load trained model
-
+    vectorizer = pickle.load(open(vectorizer_path, 'rb'))
+    model = pickle.load(open(model_path, 'rb'))
     # --------------------------------- #
-    return None
-
+    return model, vectorizer
 
 app = Flask(__name__)
 api = Api(app)
-model = load_model(conf.model_path)
+model, vectorizer = load_model(conf.model_path, conf.vectorizer_path)
 
 
 @app.route('/messages/<string:identifier>')
@@ -35,10 +35,11 @@ def pridict_labell(identifier):
     # ------ YOUR CODE HERE ----------- #
     # model predict single label
 
-    predicted_label = 'Error: Model not loaded'
+    msg_csr = vectorizer.transform([msg['txt']])
+    predicted_label = model.predict(msg_csr)[0]
+    predicted_score = model.predict_proba(msg_csr)[0][1]
 
-    # --------------------------------- #
-    return render_template('page.html', id=identifier, txt=msg['txt'], label=predicted_label)
+    return render_template('page.html', id=identifier, txt=msg['txt'], label=predicted_label, score=predicted_score)
 
 @app.route('/feed/')
 def feed():
@@ -47,14 +48,16 @@ def feed():
     # ------ YOUR CODE HERE ----------- #
     # rank all messages and predict
 
-    predicted_label = 'Error: Model not loaded'
+    all_msgs = np.array([{'msg_id': i[0], 'msg_txt': i[1]} for i in db.get_all_messages()])
+    msgs_csr = vectorizer.transform([msg['msg_txt'] for msg in all_msgs])
+    predicted_score = np.array([two_scores[0] for two_scores in model.predict_proba(msgs_csr)])
+    inds = predicted_score.argsort()
+
+    sorted_msgs = all_msgs[inds]
 
     # --------------------------------- #
-    recs = [
-        {'msg_id': 1, 'msg_txt': 'example_txt_1'},
-        {'msg_id': 2, 'msg_txt': 'example_txt_2'}
-    ]
-    return render_template('feed.html', recs=recs)
+
+    return render_template('feed.html', recs=sorted_msgs[:limit])
 
 class Messages(Resource):
     def __init__(self):
