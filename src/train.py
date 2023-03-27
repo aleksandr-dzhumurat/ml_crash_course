be6d1308da79184a445164f363a35131e0c61ec5
AndrewTr03 <62745833+AndrewTr03@users.noreply.github.com>
2023-03-27 03:52:14 +0300

    Turn crap into gold

--- src/train.py
+++ src/train.py
@@ -1,19 +1,13 @@
-"""
-----------------------------
-Train model
-----------------------------
-"""
-
 import os
 from typing import Dict
 
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.metrics import f1_score
 
 from utils import conf, logger
-
+import pickle
 
 if __name__ == '__main__':
     input_file = conf.raw_data_file
@@ -32,18 +26,26 @@ if __name__ == '__main__':
     # fit 
     vectorizer = TfidfVectorizer(**conf.tf_idf_params).fit(X_train)
     X_train_csr = vectorizer.transform(X_train)
-    lr = LogisticRegression().fit(X_train_csr, y_train)
+    # ------ YOUR CODE HERE ----------- #
+    # train better model
+    
+    gbr = GradientBoostingRegressor(random_state=1, n_estimators=500, max_depth=8).fit(X_train_csr, y_train)
     # predict
     X_test_csr = vectorizer.transform(X_test)
-    y_pred = lr.predict(X_test_csr)
+    y_pred = gbr.predict(X_test_csr)
+    y_pred = [0 if i < 0.5 else 1 for i in y_pred]
     cur_score = f1_score(y_true, y_pred)
 
     logger.info('best_score %.5f', cur_score)
 
-    # ------ YOUR CODE HERE ----------- #
-    # train better model
-
     # safe better model
+    
+    pkl_filename = conf.vector_path
+    with open(pkl_filename, 'wb') as file: 
+        pickle.dump(vectorizer, file)
+    
+    pkl_filename = conf.model_path 
+    with open(pkl_filename, 'wb') as file: 
+        pickle.dump(gbr, file)
 
-    model_path = conf.model_path
     # --------------------------------- #
